6a0f4443be3eaa3b6be7908888241533c8a3581a
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-01 18:14:04 +0300

    program2.4

---
+++ program2.4/program2.4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 4; ++i)
+            {
+     
+                    Console.Write(" " + i );
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= 3; ++i)
+            {
+
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= 2; ++i)
+            {
+
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= 1; ++i)
+            {
+
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= 0; ++i)
+            {
+
+                Console.Write(" " + i);
+            }
+        }
+    }
+}
