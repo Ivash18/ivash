e9860b2e08ef9c014fd63196eba7bb61b7b4523f
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-01 17:21:43 +0300

    program 2.3

---
+++ program2.3/program2.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапазод от A до B(целые числа)");
+            int A = int.Parse(Console.ReadLine());
+            
+            int B = int.Parse(Console.ReadLine());
+            int k = A;
+            while (k != B + 1)
+            {
+                if (k >= 0)
+                    Console.Write(k + " ");
+                k++;
+            }
+            k = A;
+            Console.WriteLine();
+            do
+            {
+                if (k >= 0)
+                    Console.Write(k + " ");
+                k++;
+            }
+            while (k != B + 1);
+            Console.WriteLine();
+            for (int i = A; i <= B; i++)
+            {
+                if (i >= 0)
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
