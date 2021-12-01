d556126b9fe05552ad4dd73d392537ad97473ce3
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-01 13:35:14 +0300

    program2.1
    program2.1

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+internal class Program
+{
+    private static void Main()
+    {
+        do
+        {
+            Console.Write("x = ");
+            var x = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.Write("y = ");
+            var y = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.WriteLine($"A({x}, {y}) => {ContainsPoint(x, y)}");
+        } while (true);
+        Console.ReadKey();
+    }
+    private static string ContainsPoint(double x, double y)
+    {
+        var values = new List<double> {
+           -(x - 70),
+           x - y,
+           y
+       };
+        if (values.Any(v => v < 0))
+            return "Нет";
+        return values.Any(v => Math.Abs(v) < 0.01) ? "На границе" : "Да"; 
+
+    }
+
+}
