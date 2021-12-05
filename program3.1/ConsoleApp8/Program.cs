67453b2fc8dea469380016de10e8dc2f71677136
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-05 16:58:03 +0300

    program3.1

---
+++ program3.1/ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int x = int.Parse(Console.ReadLine());
+            if ((x > 999) || (x < 100))
+                Console.WriteLine("Вы ввели не трехзначное число!");
+            else
+                Console.WriteLine(res(x.ToString()));
+            Console.ReadLine();
+        }
+        static string res(string s)
+        {
+            char[] n = s.ToArray();
+            n = n.Reverse().ToArray();
+            string str = null;
+            foreach (char ch in n)
+                str = string.Concat(str, ch);
+            return str;
+        }
+    }
+}
