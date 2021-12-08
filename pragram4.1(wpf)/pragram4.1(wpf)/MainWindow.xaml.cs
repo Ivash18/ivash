b649e7fd3647883f0ca42e3436d27b0157ba5019
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-08 11:59:12 +0300

    program4.1(wpf)

---
+++ pragram4.1(wpf)/pragram4.1(wpf)/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pragram4._1_wpf_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double B(int n)
+            {
+                if (n < 1) throw new ArgumentOutOfRangeException();
+                return n == 1 ? 5 : B(n - 1) / ((n - 1) * (n - 1) + n);
+            }
+            int N;
+            N = Convert.ToInt32(n.Text);
+            for (int i = 1; i <= N; ++i)
+            {
+                answer.Text = Convert.ToString((i,"=", B(i)));
+            }
+           
+        }
+    }
+}
