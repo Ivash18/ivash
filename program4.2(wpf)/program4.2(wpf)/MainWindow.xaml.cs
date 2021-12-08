b6421a1e4d56e0709d2d49aa40226d029139e170
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-08 12:00:07 +0300

    program4.2(wpf)

---
+++ program4.2(wpf)/program4.2(wpf)/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace program4._2_wpf_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            void Main()
+            {
+
+                int N;
+                N = Convert.ToInt32(n.Text);
+                Foo(N);
+            }
+            void Foo(int N1, int symbols = 1)
+            {
+                for (int i = N1; i > symbols; i--)
+                {
+                    answer.Text = Convert.ToString('*');
+                }
+                answer.Text = Convert.ToString(' ');
+                
+                if (N1 > 0)
+                    Foo(N1 - 1);
+            }
+        }
+    }
+}
