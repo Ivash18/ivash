c76fa5ad72ef17f08331e36b125dc8ef035e6ac9
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-08 11:54:53 +0300

    program4.2

---
+++ program4.2/program4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Foo(n);
+        Console.ReadKey();
+    }
+    static void Foo(int N, int symbols = 1)
+    {
+        for (int i = N; i > symbols; i--)
+        {
+            Console.Write('*');
+        }
+        Console.Write(' ');
+        Console.WriteLine();
+        if (N > 0)
+            Foo(N - 1);
+    }
+}
