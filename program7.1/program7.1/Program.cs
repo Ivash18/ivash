e7a3e04cb87d8da5e2a7b6fcfa9237ded914e815
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-11 14:46:21 +0300

    program7.1

---
+++ program7.1/program7.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_number = Console.ReadLine();
+            char[] char_arr = str_number.ToCharArray();
+            Console.WriteLine("Количество цифр в строке: " + char_arr.Where((number) => number >= '0' && number <= '9').Count());
+            Console.ReadKey();
+        }
+    }
+}
