54972ff221c25b89883b08b0a9a2937b6f655ce1
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-11 14:49:23 +0300

    program7.2(wpf)

---
+++ program7.2(wpf)/program7.2(wpf)/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace program7._2_wpf_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            void Main(string[] args)
+            {
+                string s;
+                s = Convert.ToString(ses.Text);
+                char[] seps = { ' ', '.', ',', '!', '?', '-' };
+                string[] parts = s.Split(seps);
+                int min = 1000;
+                int x = 0;
+                for (int i = 0; i < parts.Length; i++)
+                {
+                    if (parts[i].Length <= min)
+                    {
+                        min = parts[i].Length;
+                        x = i;
+                    }
+                }
+                answer.Text = Convert.ToString("Самое короткое слово: " + parts[x]);
+            }
+        }
+    }
+}
