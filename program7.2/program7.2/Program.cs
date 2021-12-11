6fc843c9e97f5c65907e6ee4751ce120d3f0ceef
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-11 14:48:00 +0300

    program7.2

---
+++ program7.2/program7.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9_11_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string s;
+            s = Console.ReadLine();
+            char[] seps = { ' ', '.', ',', '!', '?', '-' };
+            string[] parts = s.Split(seps);
+            int min = 1000;
+            int x = 0;
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i].Length <= min)
+                {
+                    min = parts[i].Length;
+                    x = i;
+                }
+            }
+            Console.WriteLine("Самое короткое слово: " + parts[x]);
+
+        }
+    }
+}
