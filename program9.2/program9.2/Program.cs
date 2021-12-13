0d339f6fef2f7fe1a583db64726174c3afbc7ea0
Ivash18 <vanya.kovrizhkin@mail.ru>
2021-12-13 18:57:57 +0300

    program9.2

---
+++ program9.2/program9.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите заданную букву: ");
+            int a = int.Parse(Console.ReadLine());
+            StreamReader fileIn = new StreamReader("C:/Users/vanya/OneDrive/Рабочий стол/out.txt");
+            string text = fileIn.ReadToEnd(); 
+
+            
+            
+            string[] newText = Regex.Split(text, "[ ,.:;]+");
+           
+            for (int i = 0; i < newText.Length; i++)
+            {
+                if(newText.Length < a) Console.WriteLine(newText[i]);
+            }
+        }
+    }
+}
